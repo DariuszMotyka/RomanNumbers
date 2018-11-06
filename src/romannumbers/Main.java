ef5b532ecc93adbdb3ac63f5464ba9aa20799f47
Dario <dmotyka1990@gmail.com>
2018-11-06 12:16:31 +0100

    version from www.algorytm.org

--- src/romannumbers/Main.java
+++ src/romannumbers/Main.java
@@ -3,8 +3,26 @@ package romannumbers;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Calculate calculator = new Calculate();
+		
+		String roman;
+		 
+		roman = calculator.arabic2roman(1981);
+		System.out.println(calculator.roman2arabic(roman) + " = " +  roman);
+		roman = calculator.arabic2roman(1);
+		System.out.println(calculator.roman2arabic(roman) + " = " +  roman);
+		roman = calculator.arabic2roman(3);
+		System.out.println(calculator.roman2arabic(roman) + " = " +  roman);
+		roman = calculator.arabic2roman(4);
+		System.out.println(calculator.roman2arabic(roman) + " = " +  roman);
+		roman = calculator.arabic2roman(5);
+		System.out.println(calculator.roman2arabic(roman) + " = " +  roman);
+		roman = calculator.arabic2roman(6);
+		System.out.println(calculator.roman2arabic(roman) + " = " +  roman);
+		roman = calculator.arabic2roman(45);
+		System.out.println(calculator.roman2arabic(roman) + " = " +  roman);
+		roman = calculator.arabic2roman(68);
+		System.out.println(calculator.roman2arabic(roman) + " = " +  roman);
 	}
 
 }
